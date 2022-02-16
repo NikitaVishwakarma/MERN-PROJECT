9600ab6d7aef413e9b668b1fd297200f3ba4bb17
Nikita Vishwakarma <nikitavishwakarma2140@gmail.com>
2022-02-16 19:25:04 +0530

    make login.js

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,9 +1,68 @@
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { Modal } from "antd";
+import Link from "next/link";
+import AuthForm from "../components/forms/AuthForm";
+import { useRouter } from "next/router";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const { data } = await axios.post(
+        `${process.env.NEXT_PUBLIC_API}/login`,
+        {
+          email,
+          password,
+        }
+      );
+      router.push("/");
+      setEmail("");
+      setPassword("");
+      setLoading(false);
+    } catch (err) {
+      toast.error(err.response.data);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="container">
-      <div className="row">
+    <div className="container-fluid">
+      <div className="row py-5 bg-default-image text-center">
         <div className="col">
           <h1>Login page</h1>
+        </div>
+      </div>
+      <div className="row py-5">
+        <div className="col-md-6 offset-md-3">
+          <AuthForm
+            handleSubmit={handleSubmit}
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            loading={loading}
+            page="login"
+          />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <p className="text-center">
+            Not yet registered?{" "}
+            <Link href="/register">
+              <a>Register</a>
+            </Link>
+          </p>
         </div>
       </div>
     </div>
